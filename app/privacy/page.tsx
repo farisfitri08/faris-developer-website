4162508f19beed9d2126d965f1f3de7af1ff5293
Faris <farisfitri08@gmail.com>
2026-09-22 09:53:46 +0800

    add jimin

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -26,6 +26,7 @@ export default function PrivacyPolicy() {
             <li>Mbappe Fake Video Call &amp; Chat</li>
             <li>Mohamed Salah Fake Video Call &amp; Chat</li>
             <li>Tom Holland Fake Video Call &amp; Chat</li>
+            <li>Park Ji-min Holland Fake Video Call &amp; Chat</li>
           </ul>
 
           <p className="mt-4 leading-7 text-slate-600">
